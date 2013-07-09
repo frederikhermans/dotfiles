b6371b54aa5baf081e421ebe82346d5a4473b9aa
Frederik Hermans <frederik.hermans@gmail.com>
2013-07-09 14:08:57 +0200

    ln on MacOS doesn't have a -b(ackup) option, ignore already linked files.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-ln -vsb $PWD/.vimrc $HOME/.vimrc
-ln -vsb $PWD/.vim $HOME/.vim
+backup() {
+
+    BACKUP="$1~"
+
+    if [ -f $1 ]
+    then
+        BACKUP="$1~"
+        SUFFIX=1
+        while [ -f $BACKUP ]
+        do
+            BACKUP="$1~.$SUFFIX"
+            let SUFFIX=SUFFIX+1
+        done
+
+        mv -v $1 $BACKUP
+    fi
+}
+
+for FILE in .vimrc .vim .gitconfig
+do
+    if [ -L $HOME/$FILE ] && [ "`readlink $HOME/$FILE`" == "$PWD/$FILE" ]
+    then
+        echo $FILE already points to $PWD/$FILE
+    else
+        backup $HOME/$FILE
+        ln -vs $PWD/$FILE $HOME/$FILE
+    fi
+done
