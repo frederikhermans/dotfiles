b9e77dc2e9bf143f5be904a90cc280cd1bdd5d45
Frederik Hermans <frederik.ac.hermans@gmail.com>
2013-07-16 15:32:35 +0200

    Add monitorlib to path.

--- profile_marsta/startup/20-import.py
+++ profile_marsta/startup/20-import.py
@@ -6,6 +6,8 @@ import tables
 import os.path
 
 sys.path.append(os.path.expanduser('~') + '/testbed/marsta/analysis')
+sys.path.append(os.path.expanduser('~') + '/testbed/python_monitorlib')
+
 import data_model
 import load
 import match
