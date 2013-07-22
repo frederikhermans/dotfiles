39fc1bcc2941161706c624a2092f41a58845239e
Frederik Hermans <frederik.ac.hermans@gmail.com>
2013-07-22 16:41:10 +0200

    Add remote dir to syspath

--- profile_marsta/startup/20-import.py
+++ profile_marsta/startup/20-import.py
@@ -7,6 +7,8 @@ import os.path
 
 sys.path.append(os.path.expanduser('~') + '/testbed/marsta/analysis')
 sys.path.append(os.path.expanduser('~') + '/testbed/python_monitorlib')
+sys.path.append(os.path.expanduser('~') + '/testbed-rodion/marsta/analysis')
+sys.path.append(os.path.expanduser('~') + '/testbed-rodion/python_monitorlib')
 
 import data_model
 import load
